1eeabf02d14eb03b212822c1b14dfd464504b383
Live Kaba <livengalamulume@gmail.com>
2021-02-18 03:55:16 +0300

    maximum

--- src/App.java
+++ src/App.java
@@ -1,6 +1,15 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        int[] num = {4, 56, 24, 85, 3, 6, 9};
+        int i = 0, sum = 0;
+        while(i < num.length) {
+            if (num[i] > sum) {
+                sum = num[i];
+                
+            }
+            i++;
+        }
+        System.out.println(sum);
         
     }
 }
